9bcc5f3752dbcd7b7eca4526db81af1cea109937
Robert Weidlich <BobWeidlich@gmail.com>
2018-01-13 17:53:43 -0800

    Clean up XML code

--- cn_xml_doc/py/ut_cn_xml_doc.py
+++ cn_xml_doc/py/ut_cn_xml_doc.py
@@ -3,56 +3,41 @@
 ########################################################################
 # file:    ut_cn_xml_doc.py
 # author:  rbw
-# date:    101219.2100
+# date:    Sat Jan 13 16:17:40 PST 2018
 # purpose: Unit test for cn_xml_doc.py
+#
+# todo:    Test with a non-RSS XML file
 ########################################################################
-import os, sys
-from cn_rss_doc import CnRssDocument
+import os
+import sys
+from cn_xml_doc import CnXmlDocument
+
 
 def main():
-  cd = os.getcwd()
-  print "current directory: " + cd
+    cd = os.getcwd()
+    print "current directory: " + cd
 
-  data_dir = "../../resources/data/"
-  path_01 = data_dir + "cn_rss_raw-news.latimes-20180113.2304.13.xml"
-  path_02 = data_dir + "cn_rss_raw-news.sweden.thelocal-20180113.2304.13.xml"
-  path_03 = data_dir + "cn_rss_raw-news.upi-20180113.2305.13.xml"
-  path_04 = data_dir + "cn_rss_raw-news.washpost.world-20180113.2307.13.xml"
+    data_dir = "../../resources/data/"
+    path_01 = data_dir + "cn_rss_raw-news.latimes-20180113.2304.13.xml"
+    path_02 = data_dir + "cn_rss_raw-news.sweden.thelocal-20180113.2304.13.xml"
+    path_03 = data_dir + "cn_rss_raw-news.upi-20180113.2305.13.xml"
+    path_04 = data_dir + "cn_rss_raw-news.washpost.world-20180113.2307.13.xml"
 
-  path = path_01
+    path = path_01
 
-  print 'testing on file \"%s\"' % path
+    print 'testing on file \"%s\"' % path
 
-  gxp = CnRssDocument(path, False)
-  #gxp = CnRssDocument(path, True)
+    gxp = CnXmlDocument(xml_file_arg=path, debug_arg=False)
+    print('###Items###')
+    nodes = gxp.getNodes()
+    for node in nodes:
+        print('########')
+        print('node:')
+        print(node)
+        print('########')
 
-  print 'type: %s' % gxp.getRssOrAtom()
+    sys.exit(0)
 
-  print('###Items###')
-  items = gxp.getItems()
-  for item in items:
-    #print('########')
-    #print('item:')
-    #print(item)
-    #print('########')
-    print('title:')
-    print(' %s' % item['title'])
-    print('text:')
-    print(' %s' % item['text'])
-    print('guid:')
-    print(' %s' % item['guid'])
-    print('pubdate:')
-    print(' %s' % item['pubdate'])
-    print('url:')
-    print(' %s' % item['url'])
-    print('author:')
-    print(' %s' % item['author'])
-    print('summary:')
-    print(' %s' % item['summary'])
-
-  sys.exit(0)
 
 if __name__ == '__main__':
-
-  main()
-
+    main()
