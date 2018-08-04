ab21f594f5f8f03f3247112e9cd65ec636cbfd68
Robert Weidlich <BobWeidlich@gmail.com>
2018-08-03 17:13:14 -0700

    New Kafka test utilities

---
+++ utils/kafka/test_kafka_consumer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+########################################################################
+# file:    test_kafka_consumer.py
+# author:  rbw
+# date:    Fri Aug  3 17:11:07 PDT 2018
+# purpose: implement a simple Kafka consumer.
+########################################################################
+
---
+++ utils/kafka/test_kafka_producer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+########################################################################
+# file:    test_kafka_producer.py
+# author:  rbw
+# date:    Fri Aug  3 17:11:07 PDT 2018
+# purpose: implement a simple Kafka producer.
+########################################################################
+
