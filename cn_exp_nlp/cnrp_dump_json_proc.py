d9ad5b919840b84414c624b85cada242b0f8a759
Robert Weidlich <BobWeidlich@gmail.com>
2018-10-14 11:38:16 -0700

    Progress to date

---
+++ cn_exp_nlp/cnrp_dump_json_proc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# file:    cnrp_dump_json_proc.py
+# author:  rbw
+# date:    Fri Oct 12 18:09:04 PDT 2018
+# purpose: convert JSON textual data to textual data
+########################################################################
