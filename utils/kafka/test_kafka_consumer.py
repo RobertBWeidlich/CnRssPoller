25547412b0b9841b4731fa816bb48bcdc58b6dbf
Robert Weidlich <BobWeidlich@gmail.com>
2018-11-02 06:53:13 -0700

    Cleaned up python kafka examples

--- utils/kafka/test_kafka_consumer.py
+++ utils/kafka/test_kafka_consumer.py
@@ -3,7 +3,7 @@
 ########################################################################
 # file:    test_kafka_consumer.py
 # author:  rbw
-# date:    Sat Aug  4 09:42:07 PDT 2018
+# date:    Thu Nov  1 16:15:57 PDT 2018
 # purpose: implement a simple Kafka consumer.
 ########################################################################
 import os
@@ -17,18 +17,28 @@ from kafka import KafkaConsumer
 from kafka.errors import KafkaError
 
 kafka_servers = ['localhost:9092']
-##kafka_topic = 'rbw-test-20180804-1105'
-#kafka_topic = 'test'
-kafka_topic = 'cnrp-nrt-feed'
+#kafka_topic = 'cnrp-nrt-feed'
+kafka_topic = 'test-20181101'
 
-consumer = KafkaConsumer(kafka_topic)
+consumer = KafkaConsumer(kafka_topic, bootstrap_servers=kafka_servers)
+print "starting Kafka consumer"
+print "Kafka servers: %s" % kafka_servers
+print "Kafka topic:   %s" % kafka_topic
+
+a = consumer.assignment()
+print "assignment: %s" % str(a)
+
+#b = consumer.beginning_offsets([0])
+#print "beginning_offsets: %s" % str(b)
+
+
 
 for msg in consumer:
     print "msg.topic:     %s" % msg.topic
     print "msg.partition: %d" % msg.partition
     print "msg.offset:    %d" % msg.offset
     print "msg.key:       %s" % msg.key
-    print "msg.value:     %s" % ""
-    print "%s" % msg.value
+    print "msg.value:     \"%s\"" % msg.value
+    #print "%s" % msg.value
     print ""
 
